ba455af9bf879663e959fbe640d2893995abe290
Tony O'Dell <tony.odell@live.com>
2012-11-05 10:20:45 -0800

    build

--- static/js/timelog.js
+++ static/js/timelog.js
@@ -1,3 +1,19 @@
-if(!window.localStorage){
-  $("#timeform > p.localstoragewarning").show();
-}
+var c = function(){ return new (function(){
+  this.g = function(key){
+    return $.cookie(key);
+  };
+
+  this.s = function(key,value){
+    $.cookie(key,value);
+  };
+
+  this.r = function(key){
+    $.removeCookie(key);
+  };
+})(); };
+
+
+$("#starttimer").click(function(){
+  c.s("timers",JSON.parse(c.g("timers")).push({title:$("#timername").val(),start:new Date()}));
+  alert(c.g("timers"));
+});
