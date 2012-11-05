0b605d1c9447448fc83b40a9bb47f20c1cb28d1e
Tony O'Dell <tony.odell@live.com>
2012-11-05 09:54:31 -0800

    build

--- static/js/ajaxsparta.js
+++ static/js/ajaxsparta.js
@@ -13,4 +13,21 @@ $(document).ready(function(){
       }
     });
   });
+
+
+  $("#createtask").click(function(e){
+    e.preventDefault();
+    $.ajax({
+      url:"/admin?action=createtask&name="+$("#taskform").find("#name").val()+"&projectname="+$("#projectname").val()+"&json=true"
+      ,success:function(data,status,xhr){
+        alert(data);
+        if(data && data.taskerror){
+          $("form#taskform > .error").remove();
+          $("form#taskform").prepend(data.taskerror);
+        }else{
+          document.location.reload(true);
+        }
+      }
+    });
+  });
 });
