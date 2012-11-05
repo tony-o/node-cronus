984a0273980b198a14670683c82469eb47ad0835
Tony O'Dell <tony.odell@live.com>
2012-11-05 10:53:36 -0800

    build

--- static/js/timelog.js
+++ static/js/timelog.js
@@ -15,7 +15,10 @@ var c = new (function(){
 $("#timeform").submit(function(e){e.preventDefault();return false;});
 
 $("#starttimer").click(function(){
-  var arr = (JSON.parse(c.g("timers"))||[]);
+  var arr = [];
+  try{
+    arr = JSON.parse(c.g("timers"));
+  }catch(e){ }
   if(!(arr instanceof Array)){ arr = []; }
   arr.push({title:$("#timername").val(),project:$("#projectname").val(),task:$("#taskname").val(),start:new Date()});
   c.s("timers",JSON.stringify(arr));
