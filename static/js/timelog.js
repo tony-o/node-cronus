6ee190ac8aa09b81627c7ebe79aab08d6ac6e1af
Tony O'Dell <tony.odell@live.com>
2012-11-05 13:13:17 -0800

    build

--- static/js/timelog.js
+++ static/js/timelog.js
@@ -27,8 +27,10 @@ var reparsetimers = function(){
       $(buffer).find(".name").text(qq[q].title);
       $(buffer).find(".project").text(qq[q].project);
       $(buffer).find(".task").text(qq[q].task);
-      var dd214 = parseFloat(qq[q].time) || 0;
-      dd214 += Math.floor(Math.abs(new Date() - new Date(qq[q].start))/1000/60);
+      var dd214 = parseFloat(qq[q].duration) || 0;
+      if(qq[q].running){
+        dd214 += Math.floor(Math.abs(new Date() - new Date(qq[q].start))/1000/60);
+      }
       $(buffer).find(".time").text(Math.floor(dd214/60) + "." + ((dd214%60)/60));
       $(buffer).find(".toggletimer").text(qq[q].running?"Stop Timer":"Start Timer");
       $(buffer).find(".index").text(q);
