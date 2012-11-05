093265233c9a6986754339e619006ae2cd54fec4
Tony O'Dell <tony.odell@live.com>
2012-11-05 00:02:34 -0800

    build

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ var db = orm.connect("mysql://"+process.env.DBUSER+":"+process.env.DBPASS+"@"+pr
   }
   console.log("Creating models");
   /* DEFINE DB MODELS */
+  var projectnamevalidation = function(val,n){return val&&val.length<3?n("too-short"):n();};
   var projectitem = db.define("project",{
-    "name":{"type":"string","validations":[orm.validators.unique()]}
+    "name":{"type":"string","validations":[orm.validators.unique(),projectitemvalidation]}
     ,"archived":{"type":"bool"}
   });
   var taskitem = db.define("task",{
