4a083fc819f73817e837f8870089d67190bc5e25
Tony O'Dell <tony.odell@live.com>
2012-11-05 21:17:53 -0800

    build

--- app.js
+++ app.js
@@ -202,6 +202,6 @@ var db = orm.connect("mysql://"+process.env.DBUSER+":"+process.env.DBPASS+"@"+pr
     
   });
   var port = process.env.PORT || 5000;
-  app.listen(port,{log:false});
+  app.listen(port);
 
 });
