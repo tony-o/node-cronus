508722ed6341a4f369eb67297522eabcac40057c
Tony O'Dell <tony.odell@live.com>
2012-11-12 13:29:09 -0800

    build

--- static/js/timelog.js
+++ static/js/timelog.js
@@ -150,7 +150,7 @@ $("#timeform").submit(function(e){e.preventDefault();return false;});
 $("#starttimer, #savetime").click(function(){
   var arr = c.g("timers");
   arr = arr instanceof Array ? arr : []; 
-  arr.push({title:$("#timername").val(),duration:$("#timertime").val(),projname:$("#projectname option:selected").text(),taskname:$("#taskname option:selected").text(),project:$("#projectname").val(),task:$("#taskname").val(),timedate:new Date(),start:new Date(),notes:$("#timernotes").val(),id:-1,running:$(this)[0]==$("#savetime")[0]?false:true,"status":"open"});
+  arr.push({title:$("#timername").val(),duration:$("#timertime").val(),projname:$("#projectname option:selected").text(),taskname:$("#taskname option:selected").text(),project:$("#projectname").val(),task:$("#taskname").val(),timedate:$("#timedate").val(),start:new Date(),notes:$("#timernotes").val(),id:-1,running:$(this)[0]==$("#savetime")[0]?false:true,"status":"open"});
   c.s("timers",JSON.stringify(arr));
   reparsetimers();
   $("#timername").val("");
